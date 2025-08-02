0f3b55e6820ead5dab6b42b2926068a9d712b4c8
arbiumbot <ostapcukdenis02@gmail.com>
2025-08-02 14:59:12 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,15 +3,12 @@ import requests
 
 app = Flask(__name__)
 
-PROXY_API_KEY = "63f98a2c-caac-4b4b-9523-58ce5350d486"
-PROXY_URL = f"https://proxy.scrapeops.io/v1/?api_key={PROXY_API_KEY}&url="
-
 BINANCE_BASE_URL = "https://fapi.binance.com"
 
 @app.route("/exchange-info")
 def get_exchange_info():
     try:
-        url = PROXY_URL + BINANCE_BASE_URL + "/fapi/v1/exchangeInfo"
+        url = f"{BINANCE_BASE_URL}/fapi/v1/exchangeInfo"
         response = requests.get(url, timeout=10)
         response.raise_for_status()
         return jsonify(response.json())
@@ -20,7 +17,7 @@ def get_exchange_info():
 
 @app.route("/")
 def home():
-    return "Binance API через ScrapeOps Proxy працює"
+    return "Binance API напряму працює"
 
 if __name__ == "__main__":
     app.run(debug=True)
